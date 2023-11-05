99ef41096a61806add41f63ddac4ea6e513350fd
Dell <habibnasir23@gmail.com>
2023-11-05 06:54:48 -0600

    Made small changes.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -65,5 +65,10 @@ namespace NewsWorld.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Map()
+        {
+            return View();
+        }
     }
 }
